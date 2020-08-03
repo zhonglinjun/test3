94f3050079d12ded851bda0e6b6017eddaedfbd3
zhonglinjun <zhonglinjun@users.noreply.github.com>
2020-08-03 15:50:30 +0800

    Update LoginServlet.java

--- src/com/yr/servlet/LoginServlet.java
+++ src/com/yr/servlet/LoginServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1134L;
+	private static final long serialVersionUID = 1L;
 	
 	
 
